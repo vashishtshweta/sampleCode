f9a4f419173acf3a71610a9246e7918964937f9a
Shaveta Vashisht <Shaveta.Vashisht@DRS.co.uk>
2021-07-07 22:02:46 +0100

    test data

---
+++ WebApplication/WebApplication/Features/Home/ErrorViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Features.Home
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+		public string ErrorMessage { get; set; }
+	}
+}
---
+++ WebApplication/WebApplication/Features/PrimeNumber/PrimeNumberController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebApplication.Features.PrimeNumber
+{
+    public class PrimeNumberController : Controller
+    {
+        private readonly ILogger<PrimeNumberController> _logger;
+
+        public PrimeNumberController(ILogger<PrimeNumberController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
